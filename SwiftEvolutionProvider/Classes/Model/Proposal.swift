c2db8daa9538ddbe8dc06c6f68c0e562cded2aa7
Pete Smith <peadar81@gmail.com>
2017-02-18 17:04:52 +0000

    Add proposal link

--- SwiftEvolutionProvider/Classes/Model/Proposal.swift
+++ SwiftEvolutionProvider/Classes/Model/Proposal.swift
@@ -13,6 +13,7 @@ public struct Proposal {
     public let title: String
     public let status: Status
     public let authors: [Author]
+    public let link: String
 }
 
 public extension Proposal {
@@ -21,16 +22,18 @@ public extension Proposal {
         static let idKey = "id"
         static let titleKey = "title"
         static let statusKey = "status"
+        static let linkKey = "link"
         static let authorsKey = "authors"
         static let authorNameKey = "name"
-        static let authorLinkKey = "link"
     }
     
     init(withJson json: [String:Any]) {
         
         let id = json[InternalConstants.idKey] as? String ?? "Id unknown"
         
-        let title: String = json[InternalConstants.titleKey] as? String ?? "Title unknown"
+        let title = json[InternalConstants.titleKey] as? String ?? "Title unknown"
+        
+        let link = json[InternalConstants.linkKey] as? String ?? ""
         
         var status = Status(version: nil, state: .unknown)
         if let statusJson = json[InternalConstants.statusKey] as? [String:String] {
@@ -42,12 +45,12 @@ public extension Proposal {
             for author in authorsData {
                 
                 let authorName = author[InternalConstants.authorNameKey] ?? "Author unknown"
-                let authorLink: String? = author[InternalConstants.authorLinkKey] ?? nil
+                let authorLink: String? = author[InternalConstants.linkKey] ?? nil
                 
                 authors.append(Author(name: authorName, link: authorLink))
             }
         }
         
-        self = Proposal(id: id, title: title, status: status, authors: authors)
+        self = Proposal(id: id, title: title, status: status, authors: authors, link: link)
     }
 }
